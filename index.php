5a9df33e9fe3b750806fa8c54d5cb378f5ecda98
Doug Black Jr <dougblackjr@gmail.com>
2024-06-17 19:43:42 -0400

    Initial

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bug with a Big Ass</title>
+    <style>
+        body {
+            background-color: #FFD700;
+            color: #0000FF;
+            font-family: Comic Sans MS, sans-serif;
+            text-align: center;
+        }
+        .container {
+            border: 5px dotted #FF00FF;
+            padding: 20px;
+            margin: 20px auto;
+            width: 90%;
+            background-color: #FFFFFF;
+        }
+        h1 {
+            color: #FF4500;
+            font-size: 48px;
+        }
+        .marquee {
+            font-size: 32px;
+            color: #FF0000;
+            margin: 20px 0;
+        }
+        .content {
+            font-size: 24px;
+            margin-top: 20px;
+            text-align: left;
+            line-height: 1.6;
+        }
+        .gif-container {
+            margin-top: 20px;
+        }
+        .gif-container img {
+            margin: 10px;
+            width: 200px;
+            height: auto;
+        }
+        .email {
+            margin-top: 20px;
+            font-size: 28px;
+            color: #32CD32;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <h1>Welcome to Bug with a Big Ass!</h1>
+    
+    <marquee class="marquee" behavior="alternate">Watch out! Bug with a Big Ass coming through!</marquee>
+    
+    <div class="content">
+        <p>Hi there! I'm Bug, and as you might have guessed, I have a very big butt. Now, this might sound funny to you, but let me tell you, having a big behind comes with its own set of challenges.</p>
+        
+        <p>Every day is an adventure for me. My oversized rear end often gets me stuck in the most unexpected places. Whether I'm trying to squeeze under a door, crawl through a tiny crevice, or hide under a leaf, my big butt always seems to be in the way.</p>
+        
+        <p>Let me share a few of my most memorable escapades:</p>
+        
+        <p><strong>The Great Escape</strong>: One sunny afternoon, I decided to explore the kitchen. The smell of delicious food was irresistible! But as I tried to scurry under the refrigerator, my butt got stuck. I wiggled and wriggled, but I just couldn't move. Eventually, the nice humans helped me out, but boy, was that embarrassing!</p>
+        
+        <p><strong>The Leaf Fiasco</strong>: Another time, I was in the garden, enjoying the fresh air. I saw a beautiful leaf and thought it would make a perfect hiding spot. Little did I know, my big butt would get me stuck once again. I had to call my bug friends to help pull me out!</p>
+        
+        <p><strong>The Door Dilemma</strong>: Doors are the worst. I remember trying to follow my friends through a crack in the door. They all made it through easily, but when it was my turn, my butt got wedged tight. I had to wait until someone opened the door to free myself. Talk about awkward!</p>
+        
+        <p>Despite all these mishaps, I love my big butt. It makes me unique and gives me lots of funny stories to tell. Plus, it keeps me warm during the chilly nights!</p>
+        
+        <p>Thanks for stopping by to hear about my adventures. If you ever see a bug struggling with a big behind, give a little help and a lot of love. And if you have any questions or just want to say hi, feel free to drop me an email!</p>
+    </div>
+    
+    <div class="gif-container">
+        <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2NrdXA2ZnljN2ppa296eHl2cWQzOWI1NGRwdzJtbHhnODMzMWdpZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TCykYFDbjYyEXtjSyJ/giphy.gif" alt="Animated Bug">
+        <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmQ0M3FsZDZobjkxMWtlNG1jaWZzaTQybG05Y2p6N2wyN2twMmx3dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rIdj5s9Ead7osqcuih/giphy.gif" alt="Animated Bug">
+        <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGZyM3hpZzQ1aThvZWMyNmRxaHJrNWdhMnNobG02eXdxbjh4aDA4NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NHljizHxKxb3y/giphy.gif" alt="Animated Bug">
+    </div>
+    
+    <div class="email">
+        <p>Email Bug: <a href="mailto:hello@bugwithabigass.com">hello@bugwithabigass.com</a></p>
+    </div>
+</div>
+
+</body>
+</html>
